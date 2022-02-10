774ddee99ddf3457bdbaf98b98e971d8e15b7526
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-02-10 16:30:20 +0500

    Message includes to-latin characters

--- src/Listeners/SendSmsNotification.php
+++ src/Listeners/SendSmsNotification.php
@@ -35,7 +35,7 @@ class SendSmsNotification
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 1,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
